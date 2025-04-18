10a0b3be8537b24d192cf703586ea9eb58d2a4e3
Josh <joshuamike@proton.me>
2025-04-18 16:33:11 -0700

    Also added .swiftformat file for custom code editing

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    var body: some View
+    {
+        VStack
+        {
+            
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading)
+            {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.black)
+                
+                HStack
+                {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("A natural rock formation resembling a turtle, Turtle Rock is a popular spot for hikers and photographers in Joshua Tree National Park.")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                
+            }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
